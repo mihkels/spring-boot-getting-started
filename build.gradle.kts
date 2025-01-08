7abe08ebdaeae0be20e1fd4e4ea3aadf9aff3670
Mihkel Selgal <mihkels@users.noreply.github.com>
2025-01-08 15:28:41 -0500

    chore: Update build.gradle.kts Spring Boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.3.5"
-    id("io.spring.dependency-management") version "1.1.6"
+  java
+  id("org.springframework.boot") version "3.4.1"
+  id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "io.github.mihkels.spring.demo"
